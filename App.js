a5bfe5bc3d1fb089463c4f9a2a88ff626e217a6a
Amber Hasan <amberhasan@Ambers-MacBook-Pro-2.local>
2023-05-18 10:57:46 -0500

    Basic todo add button and clear todo list button, all in one file

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, Button, TextInput } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function CreateTodoScreen() {
+  let [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    if (newTodo.trim() !== "") {
+      todoList = [...todoList, newTodo];
+      setTodoList(todoList); //make the current list include the todo, //setTodoList((todoList) => [...todoList, newTodo]);
+      setNewTodo(""); //make the current newTodo be empty string
+    }
+  };
+
+  const clearTodoList = () => {
+    setTodoList([]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <TextInput
+        placeholder="Enter a TODO item"
+        value={newTodo}
+        onChangeText={setNewTodo}
+        style={{
+          marginBottom: 10,
+          padding: 10,
+          borderWidth: 1,
+          borderColor: "gray",
+          width: 200,
+        }}
+      />
+      <Button title="Add TODO" onPress={addTodo} color="green" />
+      <Button title="Clear TODO List" onPress={clearTodoList} color="red" />
+      <Text>{todoList.join("\n")}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function CompletedTodoScreen() {
+  return <Text>Completed TODO</Text>;
+}
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="TODO" component={CreateTodoScreen} />
+      <Tab.Screen name="Completed" component={CompletedTodoScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
